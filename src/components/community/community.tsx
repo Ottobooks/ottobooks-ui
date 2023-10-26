bb4906a278d24a2a9f9e6ea38108b6bf361c1ba6
Praneeth Veerlapally <praneeth7516@gmail.com>
2023-10-25 20:18:12 -0400

    Fixed community css issues

--- src/components/community/community.tsx
+++ src/components/community/community.tsx
@@ -14,52 +14,135 @@ const Integrations = () => {
   };
   return (
     <div className="flex flex-col items-center justify-center px-10 py-8 gap-3">
-      <div className="font-bold text-xl">Welcome to ottobooks community</div>
+      <div className="font-bold text-xl">
+        Welcome to the ottobooks community
+      </div>
       <span>
-          Explore free and paid scripts to automate your processes faster
+        Explore thousands of free and paid scripts to automate your processes
+        faster
       </span>
       <div>
-        <input type="text" className="otto-input" placeholder="Search"></input>
+        <input
+          type="text"
+          className="otto-input"
+          placeholder="Gusto Payroll Journal Entry"
+        ></input>
       </div>
       <div className="flex gap-10 mt-8">
-        <div className="flex flex-col p-2 border-2 border-stone-700 bg-white rounded-lg h-72 w-42">
-           <div className="font-bold text-xl">Shopify Reconciliation</div>
-           <div><br/>Reconcile your Shopify orders in a breeze!</div>
-           <div><br/>Integrations: Shopify, Quickbooks</div>
-           <div className="h-20 overflow-hidden flex items-center">
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
+        <div className="flex flex-col gap-5 p-4 border-2 border-stone-700 bg-white rounded-lg h-72 w-72">
+          <div className="font-bold text-xl">Shopify Reconciliation</div>
+          <div>Reconcile your Shopify orders in a breeze!</div>
+          <div>Integrations: Shopify, Quickbooks</div>
+          <div className="flex items-center">
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
           </div>
-          <div className="otto-community-link"><a href="www.google.com">learn more</a></div>
+          <div className="flex-1 otto-community-link flex justify-end items-end">
+            <a href="www.google.com">Learn More</a>
+          </div>
         </div>
-        <div className="flex flex-col p-2 border-2 border-stone-700 bg-white rounded-lg h-72 w-42">
-           <div className="font-bold text-xl">Payroll Mate</div>
-           <div><br/>Prepare your Payroll Entries in a flash!</div>
-           <div><br/>Integrations: Gusto, ADP, Rippling, CSV</div>
-           <div className="h-20 overflow-hidden flex items-center">
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
+        <div className="flex flex-col gap-5 p-4 border-2 border-stone-700 bg-white rounded-lg h-72 w-72">
+          <div className="font-bold text-xl">Payroll Mate</div>
+          <div>Prepare your Payroll Entries in a flash!</div>
+          <div>Integrations: Gusto, ADP, Rippling, CSV</div>
+          <div className="flex items-center">
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
           </div>
-          <div className="otto-community-link"><a href="www.google.com">learn more</a></div>
+          <div className="flex-1 otto-community-link flex justify-end items-end">
+            <a href="www.google.com">Learn More</a>
+          </div>
         </div>
-        <div className="flex flex-col p-2 border-2 border-stone-700 bg-white rounded-lg h-72 w-42">
-           <div className="font-bold text-xl">Gusto Pay</div>
-           <div><br/>Automate your monthly payroll journals</div>
-           <div><br/>Integrations: Gusto</div>
-           <div className="h-20 overflow-hidden flex items-center">
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
-            <Image src="/favoicon.png" width={24} height={24} alt="Picture of the author"/>
+        <div className="flex flex-col gap-5 p-4 border-2 border-stone-700 bg-white rounded-lg h-72 w-72">
+          <div className="font-bold text-xl">Gusto Pay</div>
+          <div>Automate your monthly payroll journals</div>
+          <div>Integrations: Gusto</div>
+          <div className="flex items-center">
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
+            <Image
+              src="/favoicon.png"
+              width={24}
+              height={24}
+              alt="Picture of the author"
+            />
           </div>
-          <div className="otto-community-link"><a href="www.google.com">learn more</a></div>
-        </div> 
+          <div className="flex-1 otto-community-link flex justify-end items-end">
+            <a href="www.google.com">Learn More</a>
+          </div>
+        </div>
       </div>
     </div>
   );
