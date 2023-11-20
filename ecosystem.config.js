4b87b89d89dab46c9560f30e2faf7366a68ecd41
Shantan Reddy Marepally <mshantan@shantans-mbp-2.lan>
2023-11-19 17:11:05 -0800

    Changing next binary path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "ottobooks-ui",
-      script: "./node_modules/.bin/next/dist",
+      script: "./node_modules/next/dist/bin/next",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
