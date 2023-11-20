4121fe0fe6c99a2ba93da55b146af85feadb721e
Shantan Reddy Marepally <mshantan@shantans-mbp-2.lan>
2023-11-19 16:56:35 -0800

    testing changing the next bin path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "ottobooks-ui",
-      script: "./node_modules/next/dist/bin/next",
+      script: "./node_modules/.bin/next/dist",
       args: "start -p " + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
